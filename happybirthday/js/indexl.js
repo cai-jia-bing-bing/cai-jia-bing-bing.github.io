72831e5826b424d337533dfc9acdb3619c5a8a78
cai-jia-bing-bing <1398078323@qq.com>
2024-04-14 17:35:57 +0800

    xhx

--- happybirthday/js/indexl.js
+++ happybirthday/js/indexl.js
@@ -18,7 +18,7 @@ var S = {
             m++;
       S.Shape.render();
       //console.log(m);
-      if(m==1200){
+      if(m==2400){
         window.location.href="../html/BirthdayCake.html";
       }
     });
